3525f27587b850296f22616460d3b0bc4a9a7e26
Daniel Israel <danny.israel@gmail.com>
2024-10-20 21:43:03 -0700

    Web editor tooldbar

--- components/screens/WebEditor.tsx
+++ components/screens/WebEditor.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { View, StyleSheet, ScrollView } from 'react-native';
+import { View, ScrollView } from 'react-native';
 import 'react-quill/dist/quill.snow.css';
 
 export function WebEditor() {
@@ -15,23 +15,6 @@ export function WebEditor() {
         setContent(value);
     };
 
-    const modules = {
-        toolbar: [
-            [{ 'header': [1, 2, false] }],
-            ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-            [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
-            ['link', 'image'],
-            ['clean'],
-        ],
-    };
-
-    const formats = [
-        'header',
-        'bold', 'italic', 'underline', 'strike', 'blockquote',
-        'list', 'bullet', 'indent',
-        'link', 'image',
-    ];
-
     return (
         <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
             <View className="flex-1">
@@ -44,8 +27,6 @@ export function WebEditor() {
                                 theme="snow"
                                 value={content}
                                 onChange={handleContentChange}
-                                modules={modules}
-                                formats={formats}
                                 style={{ height: '100%' }}
                             />
                         );
